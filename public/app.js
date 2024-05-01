dadcb33522f14384543c3658261f158c76d63935
Travis Mottershead <travis.mottershead@verygoodsecurity.com>
2024-05-01 01:31:53 +0000

    Updated demo to focus on an agent portal interface.

--- public/app.js
+++ public/app.js
@@ -16,7 +16,6 @@ async function setUpApp() {
     let last4 = CARD_DATA.card_number.substr(-4);
     $('#redacted-card-number').removeClass('transparent');
     $('#redacted-card-number-last-4').html(last4);
-    $('#show-card-btn');
     $('#card-wrapper').removeClass('d-none');
     $('#server-results').removeClass('d-none')
     $('#issue-card-btn').hide();
@@ -24,86 +23,65 @@ async function setUpApp() {
     $('#good-thru-value').html(CARD_DATA.expiry)
     $('.name h3').html(CARD_DATA.name)
     securelyLoadCardNumber()
-    securelyLoadCvc()
   });
 
-  $('#show-card-btn').on('click', function () {
-    $('#show-card-btn').addClass('transparent')
-    showCardNumber();
-    showCardCvc();
-  })
+  function configureShowForRecord(row, record) {
+    let btn = row.find('[name=show-card-btn]')
+    let show = VGSShow.create(APP_CONFIG.vault_id, function (state) {
+      console.log(state);
+    }).setEnvironment('sandbox');
 
-  function securelyLoadCvc() {
-    SEC_EL_CARD_CVC = show.request({
-      name: 'secret-cvc',
-      method: 'POST',
-      path: '/reveal_card_cvc',
-      payload: { 'cvc': CARD_DATA.cvc },
-      htmlWrapper: 'text',
-      jsonPathSelector: 'cvc',
-    });
-    SEC_EL_CARD_CVC.render('#secret-cvc', {
-      fontSize: '21px',
-      color: '#fff',
-      textAlign: 'right',
-      fontFamily: 'sofia,sofiaFallback,arial,sans-serif'
+    btn.on('click', async function () {
+      securelyLoadCardNumber(show, row, record)
     });
   }
 
-  function securelyLoadCardNumber() {
-    SEC_EL_CARD_NUMBER = show.request({
-      name: 'secret-card-number',
+  async function loadRecords() {
+    let records = (await axios.get('/load_records')).data
+    let $customersTable = $('#customer-table');
+    for (let i in records) {
+      let record = records[i];
+      $row = $customersTable.find("#template").clone()
+      $row.removeClass("d-none")
+      $row.find('[name=name]').html(record.name)
+      $row.find('[name=email]').html(record.email)
+      $row.find('[name=phone]').html(record.phone)
+      $row.find('[name=card_number_token]').html(record.card_number_token)
+      $row.find('[name=card]').attr("id", "show-card-" + record.id)
+      configureShowForRecord($row, record)
+      $customersTable.append($row)
+    }
+  }
+
+  function securelyLoadCardNumber(show, row, record) {
+    let showIframe = show.request({
+      name: `show-card-${record.id}}`,
       method: 'POST',
       path: '/reveal_card_number',
-      payload: { 'card_number': CARD_DATA.card_number },
+      payload: { 'card_number': record.card_number_token },
       htmlWrapper: 'text',
       jsonPathSelector: 'card_number',
       serializers: [show.SERIALIZERS.replace('(\\d{4})(\\d{4})(\\d{4})(\\d{4})', '$1 $2 $3 $4')],
     });
-    SEC_EL_CARD_NUMBER.render('#secret-card-number', {
-      fontSize: '1.8em',
-      color: '#fff',
-      whiteSpace: 'nowrap',
+
+    row.find('[name=loading-animation]').show()
+    row.find('[name=show-card-btn]').hide()
+    showIframe.render(`#show-card-${record.id}`, {
+      color: 'black',
+      fontWeight: 'bold',
       fontFamily: 'sofia,sofiaFallback,arial,sans-serif'
     });
-    SEC_EL_CARD_NUMBER.on('revealSuccess', function() {
-      renderCopyBtn(SEC_EL_CARD_NUMBER);
+
+    showIframe.on('revealSuccess', function() {
+      row.find('[name=card_number_token]').hide()
+      row.find('[name=loading-animation]').hide()
+      setTimeout(() => {
+        showIframe.unmount()
+        row.find('[name=card_number_token]').show()
+        row.find('[name=show-card-btn]').show()
+      }, 5000)
     })
   }
-
-  function showCardNumber() {
-    $('#redacted-card-number').addClass('transparent');
-    $('#secret-card-number').removeClass('transparent');
-    $('#copy').removeClass('transparent')
-  }
-
-  function showCardCvc() {
-    $('#redacted-cvc').addClass('transparent');
-    $('#secret-cvc').removeClass('transparent');
-  }
-  function renderCopyBtn(cardNumber) {
-    const copyBtn = show.copyFrom(cardNumber,
-      {
-        text: 'Copy Card #',
-        serializers: [show.SERIALIZERS.replace(' ', '')],
-      },
-      function (status) {
-        if (status === 'success') {
-          alert('Copied!');
-        }
-      });
-    copyBtn.render('#copy', {
-      padding: '10px',
-      backgroundColor: '#35c2a1',
-      fontSize: '12px',
-      color: 'white',
-      boxShadow: '0 7px 14px 0 rgba(49, 49, 93, 0.10), 0 3px 6px 0 rgba(0, 0, 0, 0.08)',
-      fontFamily: '"Helvetica Neue", Helvetica',
-      borderRadius: '4px',
-      height: '37px',
-      outline: 'none',
-    });
-  }
-
+  loadRecords()
 }
 setUpApp()
--- server.js
+++ server.js
@@ -24,12 +24,45 @@ app.get('/get_vault_id', async (req, res) => {
     });
 });
 
-app.get('/issue_card', async (req, res) => {
-    let agent = getProxyAgent();
-    let cardDataTokens = await issueNewCard(agent);
-
+app.get('/load_records', async (req, res) => {
     res.setHeader('content-type', 'application/json')
-    res.send(cardDataTokens);
+    res.send( [
+        {
+            "id": "1000",
+            "name": "John Doe",
+            "email": "john.doe@example.com",
+            "phone": "123-456-7890",
+            "card_number_token": "424242b5qBSvXmV4242"
+        },
+        {
+            "id": "1001",
+            "name": "Jane Smith",
+            "email": "jane.smith@example.com",
+            "phone": "234-567-8901",
+            "card_number_token": "545454x5vpSoBkV5454"
+        },
+        {
+            "id": "1002",
+            "name": "Alice Johnson",
+            "email": "alice.johnson@example.com",
+            "phone": "345-678-9012",
+            "card_number_token": "411111ysBaZNuvV1111"
+        },
+        {
+            "id": "1003",
+            "name": "Bob Brown",
+            "email": "bob.brown@example.com",
+            "phone": "456-789-0123",
+            "card_number_token": "400005wse1Hc7JV5556"
+        },
+        {
+            "id": "1004",
+            "name": "Carol White",
+            "email": "carol.white@example.com",
+            "phone": "567-890-1234",
+            "card_number_token": "555555FZ2WjT3GV4444"
+        }
+    ]);
 });
 
 // Return the card cvc that was passed in.
@@ -60,55 +93,3 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-function getProxyAgent() {
-    const vgs_outbound_url = `${VGS_VAULT_ID}.sandbox.verygoodproxy.com`
-    console.log(`Sending request through outbund Route: ${vgs_outbound_url}`);
-    return tunnel.httpsOverHttps({
-        proxy: {
-            servername: vgs_outbound_url,
-            host: vgs_outbound_url,
-            port: 8443,
-            proxyAuth: `${VGS_USERNAME}:${VGS_PASSWORD}`
-        },
-    });
-}
-
-// This function simulates a request to a card issuer.
-// The request will be tunneled through a VGS Outbound Route
-// to tokenize the data during the response phase.
-// Response format:
-//   {
-//     card_number: 'tok_sandbox_pzR4KsYmCJkcyfXTPw8TrA'
-//   }
-async function issueNewCard(agent) {
-    const instance = axios.create({
-        // For this example we use the VGS echo server so that
-        // we can consistently get a card number without relying
-        // on a third-party integration.
-        // In a real card issuance environment, this request will
-        // go to the API of the service who is managing creating the
-        // card number.
-        baseURL: 'https://echo.secure.verygood.systems',
-        httpsAgent: agent,
-    });
-    
-    try {
-        // For testing purposes, we provide the card_number and cvc in the request
-        // so that we can tokenize the values it in response from the echo server.
-        // Sending encrypted data value so that we can decrypt and tokenize the values in-transit
-        let response = await instance.post(
-            '/issue_card',
-            {'card_number':'4242424242424242','cvc':'123'}
-        );
-        console.log(response.data)
-        return response.data
-    }
-    catch(error) {
-        console.log('FAILURE!');
-        console.log(error);
-        return {
-            status: 'failure',
-            error: error.message
-        }
-    }
-}
